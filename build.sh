8176bb6ab2eabb9d504d83dced63f2f4d6599148
seatle888 <seatle888@gmail.com>
2024-03-26 17:41:48 +0700

    doc: Linux 编译 flutter 文档

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@
 docker build -t flutter:v1 .
 
 # docker run -it --entrypoint /bin/bash --platform linux/amd64 6c673b80819c
+
+# flutter build linux
+# https://docs.flutter.dev/get-started/install/linux/desktop?tab=download
